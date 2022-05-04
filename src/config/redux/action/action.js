aae24fb5d7fafe4163fbb138d393dc9384d96f3f
krisna31 <krisnaaaputraaa@gmail.com>
2022-05-04 21:32:43 +0700

    another bug man in just firebase deply

--- src/config/redux/action/action.js
+++ src/config/redux/action/action.js
@@ -75,15 +75,13 @@ export const getDataFromApi = (uid) => (dispatch) => {
   return new Promise((resolve, reject) => {
     onValue(starCountRef, (snapshot) => {
       const data = [];
-      if (snapshot.val() !== null && uid !== "string") {
-        Object.keys(snapshot.val()).map((key) => {
-          data.push({
-            id: key,
-            data: snapshot.val()[key],
-          });
+      Object.keys(snapshot.val()).map((key) => {
+        data.push({
+          id: key,
+          data: snapshot.val()[key],
         });
-        dispatch({ type: "SET_NOTES", value: data });
-      }
+      });
+      dispatch({ type: "SET_NOTES", value: data });
     });
   });
 };
